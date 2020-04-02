c42127c8a48df597f97258bcfd1870845919cec0
최재용 <jayong93@gmail.com>
2020-04-02 23:09:43 +0900

    mpsc_queue 구현

---
+++ 숙제2/SimpleIOCPServer/SimpleIOCPServer/mpsc_queue.h
@@ -0,0 +1,160 @@
+#pragma once
+#include <utility>
+#include <atomic>
+#include <optional>
+#include <climits>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+struct QueueNode;
+
+struct ThreadEpoch {
+	std::atomic_ullong epoch{ ULLONG_MAX };
+	std::atomic<ThreadEpoch*> next{ nullptr };
+};
+
+template <typename T>
+struct RetiredNode {
+	RetiredNode(QueueNode<T>* node, unsigned long long epoch) : node{ node }, removed_epoch{ epoch } {}
+
+	QueueNode<T>* node{ nullptr };
+	unsigned long long removed_epoch;
+};
+
+static std::atomic_ullong g_epoch{ 0 };
+static std::atomic<ThreadEpoch*> t_epoch_list;
+static thread_local bool local_epoch_initialized = false;
+static thread_local ThreadEpoch my_epoch;
+static constexpr unsigned epoch_increase_rate = 50;
+static constexpr unsigned empty_rate = 100;
+static thread_local unsigned counter = 0;
+
+static void initialize_thread_epoch() {
+	if (local_epoch_initialized == true) { return; }
+	auto list_head = t_epoch_list.load(std::memory_order_relaxed);
+	while (true) {
+		my_epoch.next.store(list_head, std::memory_order_relaxed);
+		if (true == t_epoch_list.compare_exchange_strong(list_head, &my_epoch)) {
+			break;
+		}
+	}
+	local_epoch_initialized = true;
+}
+
+template <typename T>
+struct QueueNode {
+	QueueNode<T>() = default;
+	QueueNode<T>(T&& val) : value{ std::forward(val) } {}
+
+	T value;
+	std::atomic<QueueNode<T>*> next = nullptr;
+};
+
+template <typename T>
+// Multi-Producer, Single-Consumer Queue
+struct MPSCQueue {
+private:
+	QueueNode<T>* volatile head;
+	std::atomic<QueueNode<T>*> tail;
+	static thread_local std::vector<RetiredNode<T>> retired_list;
+
+	void start_op();
+	void end_op();
+	void retire(QueueNode<T>* node);
+	void empty();
+
+public:
+	MPSCQueue<T>() : head{ new QueueNode<T> }, tail{ head } {}
+
+	std::optional<T>&& deq();
+	void enq(T&& val);
+};
+
+template<typename T>
+inline void MPSCQueue<T>::start_op()
+{
+	initialize_thread_epoch();
+	my_epoch.epoch.store(g_epoch.load(std::memory_order_relaxed), std::memory_order_relaxed);
+}
+
+template<typename T>
+inline void MPSCQueue<T>::end_op()
+{
+	my_epoch.epoch.store(ULLONG_MAX, std::memory_order_relaxed);
+}
+
+template<typename T>
+inline void MPSCQueue<T>::retire(QueueNode<T>* node)
+{
+	retired_list.emplace_back(node, g_epoch.load(std::memory_order_relaxed));
+	counter++;
+	if (counter % epoch_increase_rate == 0) { g_epoch.fetch_add(1, std::memory_order_relaxed); }
+	if (counter % empty_rate == 0) {
+		this->empty();
+	}
+}
+
+template<typename T>
+inline void MPSCQueue<T>::empty()
+{
+	unsigned long long min_epoch = ULLONG_MAX;
+	auto thread_epoch = t_epoch_list.load(std::memory_order_relaxed);
+	while (true) {
+		auto epoch = thread_epoch->epoch.load(std::memory_order_relaxed);
+		if (min_epoch > epoch) { min_epoch = epoch; }
+
+		auto next_t_epoch = thread_epoch->next.load(std::memory_order_relaxed);
+		if (next_t_epoch == nullptr) { break; }
+		thread_epoch = next_t_epoch;
+	}
+
+	auto removed_it = std::remove_if(this->retired_list.begin(), this->retired_list.end(), [](RetiredNode<T>& r_node) {
+		if (r_node.removed_epoch < min_epoch) {
+			delete r_node.node;
+			return true;
+		}
+		return false;
+		});
+	this->retired_list.erase(removed_it, this->retired_list.end());
+}
+
+template<typename T>
+inline std::optional<T>&& MPSCQueue<T>::deq()
+{
+	start_op();
+	std::optional<T> retval;
+	QueueNode<T>* next_head = head->next.load(std::memory_order_relaxed);
+
+	if (next_head != nullptr) {
+		auto old_head = head;
+		head = next_head;
+		retire(old_head);
+		retval = std::move(next_head->value);
+	}
+
+	end_op();
+	return retval;
+}
+
+template<typename T>
+void MPSCQueue<T>::enq(T&& val)
+{
+	start_op();
+	auto new_node = new QueueNode<T>{ forward(val) };
+	QueueNode<T>* old_tail;
+	while (true) {
+		old_tail = tail.load(std::memory_order_relaxed);
+		auto old_next = old_tail->next.load(std::memory_order_relaxed);
+		if (old_next != nullptr) {
+			tail.compare_exchange_strong(old_tail, old_next);
+			continue;
+		}
+
+		if (true == old_tail->next.compare_exchange_strong(old_next, new_node)) {
+			tail.compare_exchange_strong(old_tail, old_next));
+			break;
+		}
+	}
+	end_op();
+}
